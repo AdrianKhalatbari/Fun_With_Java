96183a42d54d788287bbc891ff7059c4a0166cbf
Mohammad Khalatbari <khalatbari.computer@gmail.com>
2022-12-05 20:58:05 +0200

    Common Value in 2 lists

---
+++ PyramidCharacters.java
@@ -0,0 +1,33 @@
+package test;
+
+import java.util.Scanner;
+
+public class PyramidCharacters {
+    public static String space(double i) {
+        String output = "";
+        if (i < 1) {
+            return "";
+        }
+        for (int j = 1; j <= i; j++) {
+            output = output + (" ");
+        }
+        return output;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Please enter your number");
+        Scanner scanner = new Scanner(System.in);
+        int input = scanner.nextInt();
+        int i = 0;
+        while (i != input + 1) {
+            for (int a = input - i; a > 0; a--) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*" + " ");
+            }
+            System.out.print("\n");
+            i++;
+        }
+    }
+}
