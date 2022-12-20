7615ae99cab00c75962c919a5aa12d4aa1b238b9
Mohammad Khalatbari <khalatbari.computer@gmail.com>
2022-12-20 17:21:42 +0200

    binary search - 20-12-2022

--- BinarySearch.java
+++ BinarySearch.java
@@ -1,39 +1,61 @@
 package test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class BinarySearch {
-    public static int binarySearch(int start, int end, int key) {
+    public static int binarySearch(List<Integer> list, int key) {
+        int start = 0;
+        int end = list.size() - 1;
         while (end >= start) {
             int mid = (start + end) / 2;
-            if (mid == key) {
-                return 1;
-            } else if (mid >= key) {
-                end = mid;
-            } else start = mid;
+            if (list.get(mid) < key) {
+                start = mid + 1;
+            } else if (list.get(mid) > key) {
+                end = mid - 1;
+            } else {
+                return mid;
+            }
         }
         return -1;
     }
 
-    public static int recursiveBinarySearch(int start, int end, int key) {
+    public static int recursiveBinarySearch(List<Integer> list, int key) {
+        int start = 0;
+        int end = list.size() - 1;
         while ((end - start) > 1) {
             int mid = (start + end) / 2;
-            if (mid > key) {
+            if (list.get(mid) > key) {
                 end = mid;
-                recursiveBinarySearch(start, end, key);
-            } else if (mid < key) {
+                List<Integer> newList = new ArrayList<>();
+                for (int i = start; i < end; i++) {
+                    newList.add(list.get(i));
+                }
+                recursiveBinarySearch(newList, key);
+            } else if (list.get(mid) < key) {
                 start = mid;
-                recursiveBinarySearch(start, end, key);
-            } else return 1;
+                List<Integer> newList = new ArrayList<>();
+                for (int i = start; i < end; i++) {
+                    newList.add(list.get(i));
+                }
+                recursiveBinarySearch(newList, key);
+            } else return mid;
         }
         return -1;
     }
 
     public static void main(String[] args) {
-//        int output = binarySearch(1, 100, 11);
-        int output = recursiveBinarySearch(1, 100, 5);
-        if (output == 1) {
-            System.out.println("True");
-        } else System.out.println("False");
+        int[] list = {12, 24, 39, 43, 50, 106, 110};
+        List<Integer> integerList = new ArrayList<>();
+        for (int i : list) {
+            integerList.add(i);
+        }
+        ////////////////////////////////// Test main method
+//        int output = binarySearch(integerList, 24);
+        ////////////////////////////////// Test Recursive method
+        int output = recursiveBinarySearch(integerList, 43);
+        if (output != -1) {
+            System.out.println("The result is in index: " + output);
+        } else System.out.println("The element is not in list!");
     }
-
-
 }
