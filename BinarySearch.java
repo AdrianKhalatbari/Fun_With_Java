18ed0179d7da437ec582b238b9a6039e00e17940
Mohammad Khalatbari <khalatbari.computer@gmail.com>
2022-12-20 16:31:27 +0200

    binary search - 19-12-2022

--- BinarySearch.java
+++ BinarySearch.java
@@ -13,8 +13,23 @@ public class BinarySearch {
         return -1;
     }
 
+    public static int recursiveBinarySearch(int start, int end, int key) {
+        while ((end - start) > 1) {
+            int mid = (start + end) / 2;
+            if (mid > key) {
+                end = mid;
+                recursiveBinarySearch(start, end, key);
+            } else if (mid < key) {
+                start = mid;
+                recursiveBinarySearch(start, end, key);
+            } else return 1;
+        }
+        return -1;
+    }
+
     public static void main(String[] args) {
-        int output = binarySearch(1, 100, 1);
+//        int output = binarySearch(1, 100, 11);
+        int output = recursiveBinarySearch(1, 100, 5);
         if (output == 1) {
             System.out.println("True");
         } else System.out.println("False");
