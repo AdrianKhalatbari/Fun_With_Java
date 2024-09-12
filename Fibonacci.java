4ab4a002f4b0072d2a480abfd6e67f1c4f90c3ff
mohammadkhalatbari <khalatbari.mokaram@gmail.com>
2024-09-12 11:09:56 +0300

    Change Fibonacci

--- Fibonacci.java
+++ Fibonacci.java
@@ -4,39 +4,29 @@ import java.util.*;
 
 public class Fibonacci {
 
-    public static List<Integer> fibonacci(int startNumber) {
+    public static List<Integer> FindFibonacci() {
         List<Integer> listFibonacci = new ArrayList<>();
-        for (int i = 0; i <= startNumber; i++) {
-            if (i == 0) {
-                listFibonacci.add(0);
-            } else if (i == 1 || i == 2) {
-                listFibonacci.add(1);
-            } else {
-                int input = (listFibonacci.get(i - 1)) + (listFibonacci.get(i - 2));
-                listFibonacci.add(input);
-            }
+        listFibonacci.add(0);
+        listFibonacci.add(1);
+        int nextValue = 1;
+        while (nextValue <= 140) {
+            listFibonacci.add(nextValue);
+            nextValue = listFibonacci.get(listFibonacci.size() - 1) + listFibonacci.get(listFibonacci.size() - 2);
         }
         return listFibonacci;
     }
 
-    public static int fibonacciRecursion(int startNumber) {
+    public static int FindFibonacciRecursion(int startNumber) {
         if (startNumber <= 1) {
             return startNumber;
         }
-        return fibonacciRecursion(startNumber - 1) + fibonacciRecursion(startNumber - 2);
+        return FindFibonacciRecursion(startNumber - 1) + FindFibonacciRecursion(startNumber - 2);
     }
 
     public static void main(String[] args) {
-        System.out.println("Please enter your number");
-        Scanner scanner = new Scanner(System.in);
-        int startNumber = scanner.nextInt();
-        System.out.println("The Fibonacci is: " + fibonacci(startNumber));
-        List<Integer> fibonacciRecursionList = new ArrayList<>();
-        while (startNumber >= 0) {
-            fibonacciRecursionList.add(fibonacciRecursion(startNumber));
-            Collections.sort(fibonacciRecursionList);
-            startNumber -= 1;
-        }
-        System.out.println("The Fibonacci Recursion is " + fibonacciRecursionList);
+        // call FindFibonacci method
+        System.out.println(FindFibonacci());
+        System.out.println("The Fibonacci Recursion is " + FindFibonacciRecursion(10));
+        
     }
 }
